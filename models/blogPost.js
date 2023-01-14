10f566c953f463de5e60c7312062d4e2774d2fac
Cazier92 <bryce.cazier@gmail.com>
2023-01-13 17:05:27 -0800

    create BlogPost model, update Profile model

---
+++ models/blogPost.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const commentSchema = new Schema({
+  author: {type: Schema.Types.ObjectId, ref: 'Profile'},
+  content: {
+    type: String,
+    required: true,
+  }
+})
+
+const blogSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  author: {type: Schema.Types.ObjectId, ref: 'Profile'},
+  comments: [commentSchema],
+  photoUrl: String,
+}, {
+  timestamps: true
+})
+
+const BlogPost = mongoose.model('Profile', blogSchema)
+
+export {
+  BlogPost
+}
--- models/profile.js
+++ models/profile.js
@@ -2,9 +2,27 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const dogSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  breed: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+  }
+}, {
+  timestamps: true
+})
+
 const profileSchema = new Schema({
   name: String,
   avatar: String,
+  dogs: [dogSchema],
+  blogPosts: [{type: Schema.Types.ObjectId, ref: 'BlogPost'}],
 }, {
   timestamps: true
 })
