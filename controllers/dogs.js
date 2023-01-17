0a2ee6313d6561cac741465a16616ebb42cec3e2
Cazier92 <bryce.cazier@gmail.com>
2023-01-17 13:54:03 -0800

    add dogs index and show views, link the show view to profile of dog owner, add corresponding routes and controllers

--- controllers/dogs.js
+++ controllers/dogs.js
@@ -6,13 +6,25 @@ function index(req, res) {
   .then(dogs => {
     res.render('dogs/index', {
       dogs,
-      title: 'Dogs'
+      title: 'Dogs',
+      // breedList: dogBreeds(dogs)
     })
   })
   .catch(err => {
     console.log(err)
     res.redirect('/')
   })
+}
+
+function dogBreeds(dogs) {
+  let dogBreedArr = []
+  dogs.forEach(dog => {
+    const dogBreed = dog.breed
+    if (dogBreedArr.some(dogBreed) === false) {
+      dogBreedArr.push(dog.breed)
+    }
+  })
+  return dogBreedArr
 }
 
 function newDog(req, res) {
@@ -67,9 +79,34 @@ function show(req, res) {
   })
 }
 
+function edit(req, res) {
+  Dog.findById(req.params.id)
+  .then(dog => {
+    res.render('dogs/edit', {
+      title: 'Edit Blog Post',
+      blog
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/blogs/show')
+  })
+}
+
+function update(req, res) {
+
+}
+
+function deleteDog(req, res) {
+
+}
+
 export {
   index,
   newDog as new,
   create,
   show,
+  edit,
+  update,
+  deleteDog as delete,
 }
--- routes/dogs.js
+++ routes/dogs.js
@@ -7,8 +7,13 @@ const router = Router()
 router.get('/', dogsCtrl.index)
 router.get('/new', isLoggedIn, dogsCtrl.new)
 router.get('/:id', dogsCtrl.show)
+router.get('/:id/edit', isLoggedIn, dogsCtrl.edit)
 
 router.post('/', isLoggedIn, dogsCtrl.create)
+
+router.put('/:id', isLoggedIn, dogsCtrl.update)
+
+router.delete('/:id', isLoggedIn, dogsCtrl.delete)
 
 export {
   router
